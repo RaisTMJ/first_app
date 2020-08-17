b45c92db69b0c0d47a538ed5cf8eb7baf6ca48e0
Mohamad rais <rais95mohamad@gmail.com>
2020-08-18 01:02:36 +0800

    change the tooltip name

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Increment  by 2',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );

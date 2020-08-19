33a090e89c8d8f828d42f096ba4f741226a9cb4f
mohamad rais <rais95mohamad@gmail.com>
2020-08-19 15:47:01 +0800

    proceed with scaffold body manipulation

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('Mohamad Rais'),), body: Text('By Default this is my text'),
+   myAnswer(){
+     print('print asdasd');
+    return 1;
 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'Whats yout favaurite color?',
+      'Whats yout favaurite animal?'
+    ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Mohamad Rais'),
+      ),
+      body: Column(
+        children: [
+          Text('The Questions'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: ()=> {print('test')},
+          ),
+          RaisedButton(child: Text(questions[1]), onPressed: myAnswer),
+          RaisedButton(child: Text(questions.elementAt(0)), onPressed: null),
+          RaisedButton(child: Text(questions[0]), onPressed: null),
+        ],
+      ),
     ));
   }
 }

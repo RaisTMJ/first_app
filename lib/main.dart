7a971dc72350f84abf8fd84b489d300955a4e284
mohamad rais <rais95mohamad@gmail.com>
2020-08-21 23:23:14 +0800

    able to click button without hit error

--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,30 @@ class myApp extends StatefulWidget {
 
 class _myAppState extends State<myApp> {
   var _questionIndex = 0;
-
+  var questions = [
+    'Whats yout favaurite color?',
+    'Whats yout favaurite animal?',
+    'Whats yout favaurite test ?',
+    'When last you poop in your trousers?',
+    'Whats yout favaurite animal?',
+    'Whats yout favaurite animal?',
+    'Whats yout favaurite animal?',
+    'Whats yout favaurite animal?',
+    'Whats yout favaurite animal?',
+    'Whats yout favaurite animal?',
+    'Whats yout favaurite animal?',
+  ];
   _myAnswer() {
     setState(() {
       _questionIndex++;
+      if (_questionIndex + 1 == questions.length) _questionIndex--;
     });
-    print(_questionIndex);
     return 1;
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'Whats yout favaurite color?',
-      'Whats yout favaurite animal?',
-      'Whats yout favaurite test ?',
-      'When last you poop in your trousers?',
-      'Whats yout favaurite animal?',
-      'Whats yout favaurite animal?',
-      'Whats yout favaurite animal?',
-      'Whats yout favaurite animal?',
-      'Whats yout favaurite animal?',
-      'Whats yout favaurite animal?',
-      'Whats yout favaurite animal?',
-    ];
+
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
@@ -54,6 +54,9 @@ class _myAppState extends State<myApp> {
                 RaisedButton(child: Text('Answer 1'), onPressed: _myAnswer),
                 RaisedButton(child: Text('Answer 2'), onPressed: _myAnswer),
                 RaisedButton(child: Text('Answer 3'), onPressed: _myAnswer),
+                RaisedButton(child: Text('Answer 3'), onPressed: _myAnswer),
+                RaisedButton(child: Text('Answer 3'), onPressed: _myAnswer),
+                RaisedButton(child: Text('Back'), onPressed: _back),
               ],
 
             ),
@@ -61,5 +64,14 @@ class _myAppState extends State<myApp> {
                 child: Text('Drawwwer')// Populate the Drawer in the next step.
             )
         ));
+  }
+
+  void _back() {
+    setState(() {
+      _questionIndex--;
+      print(_questionIndex);
+      if (_questionIndex == 0) _questionIndex=1;
+    });
+
   }
 }
